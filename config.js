b965891067e765d8586efe5e5f5d8cdf26098bf2
jaksu32 <117660988+jaksu32@users.noreply.github.com>
2022-11-08 11:26:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // const tgToken = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"; //aryantestbot
-const tgToken = "1237067579:AAGIFKPpyPk9FGD55SpKN6CgZaiW8iEj2Io"; //gtorrent
+const tgToken = "5587311521:AAFcGar2idhzJr3obs2hgrFXUmWzexYaqtI"; //gtorrent
 const downloadFolder = "downloads";
 const mongoUrl = "mongodb+srv://aryanvikash:vikash12@cluster0-mp4rw.mongodb.net/test?retryWrites=true&w=majority"
 // const maxSize = 10;
